111746cae218073739cb131d26a68e9c21849997
Kons <katashi.rain@gmail.com>
2021-07-22 16:50:09 +0900

    sorted df; next iterate for Gross negative

--- tradelog_to_csv.py
+++ tradelog_to_csv.py
@@ -2,19 +2,16 @@ import os
 import io
 import pandas as pd
 
-with open('python-finance/trades/trade.txt', 'r') as f:
+with open('tradelog_importer/trades/trade.txt', 'r') as f:
     data = f.read().replace('\n',';').replace(';;','\n')
-    #print(data)
 
-
-df = pd.read_csv(io.StringIO(data), sep=";", header=None)
-df[1] = df[1].str[4:]
-df[3] = df[3].str[4:]
-df['Open'] = pd.to_datetime(df[0] + ' ' + df[1])
-df['Close'] = pd.to_datetime(df[2] + ' ' + df[3])
-df = df.drop(columns=[0, 1, 2, 3])
-cols = df.columns.tolist()
-cols = cols[-2:] + cols[:-2]
-df = df[cols]
+hearer_list = ['Open Date', 'Open Time', 'Close Date', 'Close Time', 'Held', 'Symbol', 'Side', 'Avr Entry', 'Avr Exit', 'Shares', 'Gross', 'Comm'] 
+df = pd.read_csv(io.StringIO(data), sep=";", header=None, names=hearer_list)
+df['Open Date/Time'] = pd.to_datetime(df['Open Date'] + ' ' + df['Open Time'].str[4:]) # .str[4:] will cut first 3 char for the day of the week
+df['Close Date/Time'] = pd.to_datetime(df['Close Date'] + ' ' + df['Close Time'].str[4:])
+df = df.drop(columns=['Open Date', 'Open Time', 'Close Date', 'Close Time'])
+columns_list = df.columns.tolist()
+columns_sorted = columns_list[-2:] + columns_list[:-2] # takiing last two columns [-2:] and adding everything before that [:-2]
+df = df[columns_sorted]
 print(df)
-df.to_csv('trade.csv')
+#df.to_csv('tradelog_importer/trade.csv')
