39af039609ab406e552a8e4fb4af2f68a7d34545
Kons <katashi.rain@gmail.com>
2021-10-06 22:06:04 +0900

    adding noted for future ref

--- tradelog_IB.py
+++ tradelog_IB.py
@@ -6,12 +6,12 @@ class Executions:
         self.path = path
         self.df = pd.read_csv(path, sep='|', header=None, skiprows=5, engine='python', skipfooter=1, parse_dates=[7, 8])
         self.df = self.df.drop(columns=[0,1,3,4,5,7,9,11,13,15])
-        self.df = self.df.rename(columns={2:'Symb', 6:'Code', 8:'Date Time', 10:'Shares', 12:'Price', 14:'Comm'})
+        self.df = self.df.rename(columns={1:'ID', 2:'Symb', 6:'Code', 8:'Date Time', 10:'Shares', 12:'Price', 14:'Comm'})
         self.df = self.df.sort_values(by='Date Time')
 
 class Trades:
     def __init__(self):
-        self.df = pd.DataFrame(columns=['Symb', 'Open Pos', 'Side', 'Status'])
+        self.df = pd.DataFrame(columns=['Symb', 'Total Pos', 'Open Pos', 'Side', 'Status', 'Trade ID'])
 
     def add(self, symbol, shares):
         side = 'Long' if shares > 0 else 'Short'
@@ -46,9 +46,12 @@ for index, row in executions.df.iterrows():
     match = trades.df[condition1 & condition2]
     if match.empty:
         print('No match in the DataFrame, adding new entry')
+        # Create trade ID
+        # Add first exec ID to the key dict
         trades.add(symbol, shares)
     else:
         print('Match found, updating position')
+        # Add exec ID to key dict
         trade_index = match.index[0]
         initial_position = trades.get_position(trade_index)
         new_position = initial_position + shares
@@ -61,7 +64,9 @@ for index, row in executions.df.iterrows():
             print('Long -> flip to Short detected, adding new trade')
             trades.update_position(trade_index, 0)
             trades.update_status(trade_index, 'Closed')
+            # Change current exec shares to fit to 0 total
             trades.add(symbol, new_position)
+            # Create exec ID and add to key dict
         elif new_position > 0 and side == 'Short':
             print('Short -> flip to Long detected, adding new trade')
             trades.update_position(trade_index, 0)
@@ -72,8 +77,10 @@ for index, row in executions.df.iterrows():
 
 print(trades.df)
 
-''' Loop for counting shares
-1) For each row update the open_df_trades df_executions 
+''' 
+Sort executions by Date
+Loop for counting shares
+1) For each row update the trade df
     a) If no Symb 
         add new entry, 
         add shares, 
@@ -90,3 +97,13 @@ print(trades.df)
 
 # add trade ID and create additional column with trade ID for execution - trade match
 # sort the data by date before iteration 
+
+'''
+Supporting position flip and taking missing executions into account
+Existiong execution df
+Add new executions - keep IDs in memory
+Sort by Date, select by symbol
+For each ID from memory present in selection get min index
+Recount from prev trade (delete all messed up trades that happened due to the missing execution?)
+If no trade recount from 0th entry
+'''
